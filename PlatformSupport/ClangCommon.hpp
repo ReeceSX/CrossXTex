0b9cfa556e24922bab0d4fc63266b17d2dfbd962
J Reece Wilson <me@reece.sx>
2020-09-12 03:36:42 +0100

    Split clang header

--- PlatformSupport/ClangCommon.hpp
+++ PlatformSupport/ClangCommon.hpp
@@ -1,15 +1,4 @@
 #pragma once
 
 #include "ClangBsearch.hpp"
-
-static int memcpy_s(void * dest, size_t destsz,
-                  const void * src, size_t count)
-{
-    size_t minVal = std::min(destsz, count);
-    if (count > destsz)
-    {
-        return 1;
-    }
-    std::memcpy(dest, src, minVal);
-    return 0;
-}
+#include "ClangMemCpy_s.hpp"
---
+++ PlatformSupport/ClangMemCpy_s.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+static int memcpy_s(void * dest, size_t destsz,
+                  const void * src, size_t count)
+{
+    size_t minVal = std::min(destsz, count);
+    if (count > destsz)
+    {
+        return 1;
+    }
+    std::memcpy(dest, src, minVal);
+    return 0;
+}
